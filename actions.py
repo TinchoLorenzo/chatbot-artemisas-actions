d58b1984a954483f1bfb2663b5f4dc3cd1ef670f
mlorenzo <mlorenzo@alumnos.exa.unicen.edu.ar>
2020-12-02 09:25:11 -0300

    sa

--- actions.py
+++ actions.py
@@ -20,34 +20,6 @@ url = 'https://botdisenio.herokuapp.com/webhooks/my_connector/webhook/'
 client = pymongo.MongoClient("localhost", 27017)
 db = client.rasa
 
-def subscribe_connection_request():
-    connection = pika.BlockingConnection(
-    pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
-    channel = connection.channel()
-
-    #channel.exchange_declare(exchange='topic_logs', exchange_type='topic', durable=True)
-
-    result = channel.queue_declare(queue='', exclusive=True)
-    queue_name = result.method.queue
-
-    channel.queue_bind(exchange='topic_logs', queue=queue_name, routing_key="Chatbot.PedidoConeccion")        
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-    channel.start_consuming()
-
-subscribe_connection_request()
-
-def callback(ch, method, properties, body):
-    print(body.decode())
-    #obj = json.loads(body.decode())
-    #url = obj['url']
-    url="https://botdisenio.herokuapp.com/webhooks/my_connector/webhook/"
-    myjson = {"message": "hi","sender": "Chatbot-Artemisas"}
-    requests_response = requests.post(url, json = myjson)
-    pasarDatos(url, "TiempoLecturaUserStory")
-    pasarDatos(url, "TiempoTrabajoUserStory")
-    pasarDatos(url, "Recurso")
-    pasarDatos(url, "ParticipacionesMeetings")
 
 
 def pasarDatos(url, tipo):
@@ -79,6 +51,36 @@ def pasarDatos(url, tipo):
     }
     requests_response = requests.post(url, json = myjson)
     return "Numero de datos de {} enviados: {}\n".format(tipo, len(myjsonL['items']))
+
+def callback(ch, method, properties, body):
+    print(body.decode())
+    #obj = json.loads(body.decode())
+    #url = obj['url']
+    url="https://botdisenio.herokuapp.com/webhooks/my_connector/webhook/"
+    myjson = {"message": "hi","sender": "Chatbot-Artemisas"}
+    requests_response = requests.post(url, json = myjson)
+    pasarDatos(url, "TiempoLecturaUserStory")
+    pasarDatos(url, "TiempoTrabajoUserStory")
+    pasarDatos(url, "Recurso")
+    pasarDatos(url, "ParticipacionesMeetings")
+    
+def subscribe_connection_request():
+    connection = pika.BlockingConnection(
+    pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
+    channel = connection.channel()
+
+    #channel.exchange_declare(exchange='topic_logs', exchange_type='topic', durable=True)
+
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='topic_logs', queue=queue_name, routing_key="Chatbot.PedidoConeccion")        
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+    
+    
+subscribe_connection_request()
 
 class ActionHelloWorld(Action):
 
